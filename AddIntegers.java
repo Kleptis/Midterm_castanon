2d41248ebac203a079b74f2cbce5346bc11d1172
zakzak1248 <106563168+zakzak1248@users.noreply.github.com>
2023-12-30 20:24:20 -0500

    Update AddIntegers.java

--- AddIntegers.java
+++ AddIntegers.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;  // Import the Scanner class
+
 public class AddIntegers {
 
     public static void main(String[] args) {
+       Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+       System.out.println("Enter two numbers: " + first + " " + second);
 
-        int first = 10;
-        int second = 20;
+        try{
+            int first = Scanner.nextInt();
+            int second = Scanner.nextInt();
+        }
+        exception{
+            System.out.println("Whoops! Looks like our keyboards aren't seeing eye-to-eye today. Could you give that another try? Remember, the keyboard is mightier than the sword but not when it comes to typos!");
+        }
 
-        System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
 
         System.out.println("The sum is: " + sum);
